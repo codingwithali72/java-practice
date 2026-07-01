00e95f0672c7f83a2659aff905139de163ab3578
Your Ali <codingwithali72@gmail.com>
2026-07-01 10:05:16 +0530

    loops in java

--- src/com/ali/java/Loops.java
+++ src/com/ali/java/Loops.java
@@ -2,12 +2,12 @@ package com.ali.java;
 
 public class Loops {
     public static void main( String[] args ) {
-        int n= 4324658;
-        int digits=0;
-        while(n>0){
-            n=n/10;
-            digits++;
-            System.out.println(digits);
+        int i=6;
+        int fact=1;
+        while(i>0){
+            fact=fact*i;
+            i--;
         }
+        System.out.println(fact);
 }
 }
--- src/com/ali/java/Test.java
+++
@@ -1,7 +0,0 @@
-package com.ali.java;
-
-public class Test {
-    static void main() {
-        System.out.println("hello world");
-    }
-}
