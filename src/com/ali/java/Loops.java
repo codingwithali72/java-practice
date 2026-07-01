0a5d382bc9958d3a293ef5b7aec85a9439d74cfe
Your Ali <codingwithali72@gmail.com>
2026-07-01 06:21:42 +0530

    if-else in java

---
+++ src/com/ali/java/Loops.java
@@ -0,0 +1,18 @@
+package com.ali.java;
+
+public class Loops {
+    public static void main( String[] args ) {
+      int marks = 95;
+      if (marks >=90){
+          System.out.println("A+");
+      }else if (marks >=80){
+          System.out.println("B");
+      }else if (marks >=60){
+          System.out.println("c");
+    }else if (marks >=40) {
+          System.out.println("D");
+      } else {
+          System.out.println("you are failed");
+      }
+}
+}
--- src/com/ali/java/Test.java
+++ src/com/ali/java/Test.java
@@ -1,12 +1,7 @@
 package com.ali.java;
 
 public class Test {
-    public static void main(String[] args) {
-        String a = new String("ali");
-        String b = new String("ali");
-        String c = "ali";
-        String d = "ali";
-        System.out.println(a == c);
-        System.out.println(b == d);
+    static void main() {
+        System.out.println("hello world");
     }
 }
