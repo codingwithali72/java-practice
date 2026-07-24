b4633d3e964025b46a68eae37be6918f496e2374
Your Ali <codingwithali72@gmail.com>
2026-07-24 09:05:22 +0530

    Variables in Java level 3

--- src/com/ali/java/Test.java
+++ src/com/ali/java/Test.java
@@ -15,14 +15,34 @@ public class Test {
 //        System.out.println(city);
 
 //  Q.5)
-        String bookName="Forty Hadith";
-        String author="Ayatullah Khomeini";
-        int pages=720;
-        int price=800;
+//        String bookName="Forty Hadith";
+//        String author="Ayatullah Khomeini";
+//        int pages=720;
+//        int price=800;
+//
+//        System.out.println("Book Name: "+bookName);
+//        System.out.println("Author: "+author);
+//        System.out.println("Pages: "+pages);
+//        System.out.println("Price: "+price);
+//    Q.7)
+//      int salary=50000;
+//      int increment=10000;
+//      salary=salary+increment;
+//
+//        System.out.println("salary:"+salary);
+//     Q.9)
+//        int temp =35;
+//        int increase=4;
+//        temp=temp+increase;
+//
+//        System.out.println("temp="+temp);
+//  LEVEL 5
+        String name="Shujaat Ali Punjani";
+        int rollno=241242;
+        int age=20;
+        float CGPA=8.96f;
+        String college="M.H. Saboo Siddik College of engineering";
 
-        System.out.println("Book Name: "+bookName);
-        System.out.println("Author: "+author);
-        System.out.println("Pages: "+pages);
-        System.out.println("Price: "+price);
     }
+
 }
