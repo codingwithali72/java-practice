72554567d1569deb18c656c1ce57478ebaac1a3c
Your Ali <codingwithali72@gmail.com>
2026-07-24 08:47:46 +0530

    Variables in Java

---
+++ src/com/ali/java/Car.java
@@ -0,0 +1,52 @@
+package com.ali.java;
+
+public class Car {
+    private String color;
+    private String brand;
+    private String model;
+    private int year;
+    int speed;
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void accelerate(int increment){
+        speed+=increment;
+    }
+    public void brake(int decrement){
+        speed-=decrement;
+        if(speed<0){
+            speed=0;
+        }
+    }
+}
---
+++ src/com/ali/java/Test.java
@@ -0,0 +1,28 @@
+package com.ali.java;
+
+public class Test {
+
+    public static void main(String[] args) {
+// Q.1)
+//        String name="Ali";
+//        int age=20;
+//        float CGPA=8.96f;
+//        String city="Mumbai";
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(CGPA);
+//        System.out.println(city);
+
+//  Q.5)
+        String bookName="Forty Hadith";
+        String author="Ayatullah Khomeini";
+        int pages=720;
+        int price=800;
+
+        System.out.println("Book Name: "+bookName);
+        System.out.println("Author: "+author);
+        System.out.println("Pages: "+pages);
+        System.out.println("Price: "+price);
+    }
+}
