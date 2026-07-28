0e7b3125dfea3a9269b4c0da5888e9e1fce845b0
Your Ali <codingwithali72@gmail.com>
2026-07-28 09:07:18 +0530

    methods in java

---
+++ src/com/ali/java/methods.java
@@ -0,0 +1,21 @@
+package com.ali.java;
+
+public class methods {
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(13));
+
+    }
+
+    public static boolean isPrime(int x) {
+        int res=0;
+        for(int i=1;i<=x/2;i++){
+            if(x%i==0){
+                res++;
+            }
+        }
+      return res ==1;
+
+    }
+}
+
